f0f4c7e2756ca2ab82b8a15a2c97c5509aac39e8
Delton Ding <dsh0416@gmail.com>
2022-02-27 18:38:40 +0800

    init pinia

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
-<template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+<template lang="pug">
+el-container#main
+  el-aside(width="200px") Aside
+  el-container
+    el-header
+      el-menu(mode="horizontal", router, default-active="/")
+        el-menu-item(index="/") Performance
+        el-menu-item(disabled) Export
+        el-menu-item(disabled) Advanced
+        el-menu-item(index="/about") About
+    el-main
+      router-view
 </template>
 
 <style lang="stylus">
+body
+  margin 0
+
 #app
   font-family Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+
+#main
+  width 100%
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 import ElementPlus from "element-plus";
+import { createPinia } from "pinia";
 import "element-plus/dist/index.css";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).use(ElementPlus).mount("#app");
+createApp(App).use(router).use(createPinia()).use(ElementPlus).mount("#app");
