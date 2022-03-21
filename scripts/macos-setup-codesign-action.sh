5dd1a4a2b8e1868d4461329103755fa6b5bcd8bc
Delton Ding <dsh0416@gmail.com>
2022-03-22 03:40:51 +0800

    fail fast

--- scripts/macos-setup-codesign-action.sh
+++ scripts/macos-setup-codesign-action.sh
@@ -11,13 +11,13 @@ function error() {
 
 function cleanup {
     # Remove generated files
-    rm -f "$RUNNER_TEMP/$CERT.tmpl" "$RUNNER_TEMP/$CERT.cer" "$RUNNER_TEMP/$CERT.key" > /dev/null 2>&1
+    rm -f "$DIR/$CERT.tmpl" "$DIR/$CERT.cer" "$DIR/$CERT.key" > /dev/null 2>&1
 }
 
 trap cleanup EXIT
 
 # Create the certificate template
-cat <<EOF >$RUNNER_TEMP/$CERT.tmpl
+cat <<EOF >$DIR/$CERT.tmpl
 [ req ]
 default_bits       = 2048        # RSA key size
 encrypt_key        = no          # Protect private key
@@ -32,17 +32,17 @@ extendedKeyUsage   = critical,codeSigning
 EOF
 
 # Generate a new certificate
-openssl req -new -newkey rsa:2048 -x509 -days 3650 -nodes -config "$RUNNER_TEMP/$CERT.tmpl" -extensions codesign_reqext -batch -out "$RUNNER_TEMP/$CERT.cer" -keyout "$RUNNER_TEMP/$CERT.key" > /dev/null 2>&1
+openssl req -new -newkey rsa:2048 -x509 -days 3650 -nodes -config "$DIR/$CERT.tmpl" -extensions codesign_reqext -batch -out "$DIR/$CERT.cer" -keyout "$DIR/$CERT.key" > /dev/null 2>&1
 [ $? -eq 0 ] || error Something went wrong when generating the certificate
 
 # create temporary keychain
-KEYCHAIN_PATH=$RUNNER_TEMP/app-signing.keychain-db
+KEYCHAIN_PATH=$DIR/app-signing.keychain-db
 security create-keychain -p "KEYCHAIN_PASSWORD_EXAMPLE" $KEYCHAIN_PATH
 security set-keychain-settings -lut 21600 $KEYCHAIN_PATH
 security unlock-keychain -p "KEYCHAIN_PASSWORD_EXAMPLE" $KEYCHAIN_PATH
 
 # Install the key for the certificate in the system keychain
-sudo security import "$RUNNER_TEMP/$CERT.key" -A -k $KEYCHAIN_PATH > /dev/null 2>&1
+security import "$DIR/$CERT.key" -A -k $KEYCHAIN_PATH > /dev/null 2>&1
 [ $? -eq 0 ] || error Something went wrong when installing the key
 
 # Exit indicating the certificate is now generated and installed
