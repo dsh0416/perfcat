ecd8407a583b1e0d6323e07dfbbaaaee7166b10f
Delton Ding <dsh0416@gmail.com>
2022-09-17 16:40:20 +0800

    fixes

--- native/injector/injector/injector.hpp
+++ native/injector/injector/injector.hpp
@@ -4,12 +4,13 @@
 
 namespace perfcat {
 class IInjector {
-  public:
-    IInjector(IProcess& process, const std::string& dll_path) : dll_path_(dll_path) {};
-    virtual ~IInjector() = default;
-    virtual bool install(std::size_t size, const std::uint8_t* arg);
+public:
+  IInjector(IProcess& process, const std::vector<std::uint8_t>& library)
+      : library_(library){};
+  virtual ~IInjector() = default;
+  virtual bool install(const std::vector<uint8_t>& args);
 
-  protected:
-    const std::string& dll_path_ = "";
+protected:
+  const std::vector<std::uint8_t>& library_;
 };
-}
+} // namespace perfcat
