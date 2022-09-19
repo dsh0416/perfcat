40a00ad4bc812c9af664f02fc89151647e76b98b
Delton Ding <dsh0416@gmail.com>
2022-09-19 09:37:20 +0800

    fix

--- native/setup.sh
+++ native/setup.sh
@@ -4,12 +4,12 @@
 if [[ "$OSTYPE" == "msys" ]]; then
   git clone https://github.com/microsoft/vcpkg
   ./vcpkg/bootstrap-vcpkg.bat
-  ./vcpkg/vcpkg install -y curl openssl
-  ./vcpkg/vcpkg install -y curl:x64-windows openssl:x64-windows
+  ./vcpkg/vcpkg install curl openssl
+  ./vcpkg/vcpkg install curl:x64-windows openssl:x64-windows
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
   git clone https://github.com/microsoft/vcpkg
   ./vcpkg/bootstrap-vcpkg.sh
-  ./vcpkg/vcpkg install -y curl openssl
+  ./vcpkg/vcpkg install curl openssl
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   brew install curl openssl
   brew link --force openssl
